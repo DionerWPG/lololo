f37969021d3467c25b5ec81dc3d2597992df344e
DionerWPG <krutoy6az6@gmail.com>
2024-11-29 19:56:08 +0300

    media css and style css was been upgraded/ kartinka udalena

---
+++ script.js
@@ -0,0 +1,13 @@
+// function emailSend(){
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "krutoy6az6@gmail.com",
+//         Password : "9310C0A9378FE300EBE49835FDCF5E8CEA07",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
